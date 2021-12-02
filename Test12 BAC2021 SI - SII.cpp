08c054d2aea08e3afa22ed35eedbc4f4f7c080bb
rotti123 <92742818+rotti123@users.noreply.github.com>
2021-12-02 10:38:03 +0200

    Create Test12 BAC2021 SI - SII.cpp

---
+++ Test12 BAC2021 SI - SII.cpp
@@ -0,0 +1,25 @@
+┌pentru i<-n%k,1,-1 execută 
+│ scrie 3*t,' ' 
+└■
+
+i<-n%k
+cat timp i>=1 executa 
+   scrie 3*t,' ' 
+   i<-i-1
+sf cat timp
+
+L=[2,4,5,6]
+C=[1 2 6 3 5 6 1]
+
+1 2
+1 6
+2 4
+2 6
+3 5
+3 6
+4 5
+5 6
+
+if(strchr("OAU",s[i])!=NULL)    cout<<"*";
+else		  
+   cout<<s[i];
