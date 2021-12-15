aabe48784b5ccc1c3e2bd420b86245b4ce0d148e
rotti123 <92742818+rotti123@users.noreply.github.com>
2021-12-15 17:20:40 +0200

    Update BAC 2020 T18.cpp

--- BAC 2020 T18.cpp
+++ BAC 2020 T18.cpp
@@ -1,3 +1,62 @@
+SI--> SII
+1d 2c 3a 4d 5b
+1 2 3 4 5 6 7 8 9 10
+5,1,10,7,0,4,1,7,4,5
+
+i=1	k=3
+3 2 1 1 2 3
+i=2	k=2
+2 1 1 2
+R: 3 2 1 1 2 3 2 1 1 2 
+f(23575209) = 1 + f(235752)
+f(235752) = 0 +f(2357)
+f(2357) = 1 + f(23)
+f(23) = 1 + f(0)
+
+
+i	j	a[i][j]
+0 	0	0
+1	1	2
+2	2	0
+3	3	6
+4	4	0
+5	5	10
+//##########################################################
+SIII ex 1
+
+#include <iostream>
+
+using namespace std;
+
+void suma(int n, int &s){
+    int c;
+    s=0;
+    int vf[10]={};
+    while(n!=0){
+        c=n%10;
+        n/=10;
+        if(c%2==0){
+            vf[c]=1;
+        }
+    }
+    for(int i=0;i<=8;i+=2){
+        s=s+vf[i]*i;
+        /*
+        sau
+        if(v[i]!=0){
+            s=s+i;
+        */
+    }
+}
+
+int main()
+{
+ int n=67638825,s;
+    suma(n,s);
+    cout<<s;
+}
+
+
 #include <iostream>
 #include <cstring>
 
