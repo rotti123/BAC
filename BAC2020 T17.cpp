2e78e26cec41435ade7b6b73065d72f6ba91b222
rotti123 <92742818+rotti123@users.noreply.github.com>
2021-12-15 17:45:40 +0200

    Create BAC2020 T17.cpp

---
+++ BAC2020 T17.cpp
@@ -0,0 +1,42 @@
+SI
+1 d
+2 d
+f(2,20) -> f(3,19) 22
+f(3,19) -> f(5,18) 22
+f(5,18) -> f(9,17) 23
+f(9,17) -> f(17,16) 26
+f(17,16) -> 33
+
+3) b
+Meteora ___ _____
+A(4,2)-A(3,2) = 4!/2!-3!/1!=3*4-6=6
+
+4) a
+
+SII
+1 9
+2 8	10
+3 7
+4 6	10
+5 5
+6 4
+
+
+
+SII d)
+
+citește x,y
+ (numere naturale x≤y)
+i<-x; j<-y; s<-0
+ dacă i≤j atunci
+┌repetă
+│┌dacă i%2=0 atunci
+││   s<-s+j
+│└sf dacă
+│┌dacă j%2=0 atunci
+││   s<-s+i
+│└sf dacă
+│i<-i+1; j<-j-1
+└până când i>j
+sf dacă
+scrie s
