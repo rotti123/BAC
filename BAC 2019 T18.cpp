c988e19c81f73c83e80d6344118112c385d1bafe
romanpaaul <95294782+romanpaaul@users.noreply.github.com>
2021-12-15 16:56:51 +0200

    Create BAC 2019 T18.cpp

---
+++ BAC 2019 T18.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    char s[21][21] = {}, model[20] = {}, *p;
+    int n, k;
+    bool ok = false;
+    cin >> n >> k;
+    for(int i = 0; i < n; i++) {
+        cin >> s[i];
+    }
+    strcpy(model, s[k]);
+    for(int i = 0; i < n; i++) {
+        if(i == k) continue;
+        p = strstr(s[i], model);
+        if(p != NULL) {
+            if(strcmp(model, p) == 0) {
+                ok = true;
+                cout << s[i] << " ";
+            }
+        }
+    }
+    if(ok == false) cout << "nu exista";
+    return 0;
+}
