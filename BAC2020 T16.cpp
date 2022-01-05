854cf1e3c15d35477f49eca8d3c879eab37827ef
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-01-05 17:52:05 +0200

    Update BAC2020 T16.cpp

--- BAC2020 T16.cpp
+++ BAC2020 T16.cpp
@@ -20,3 +20,88 @@ int main() {
         cout<<endl;
     }
 }
+///SIII ex 3
+#include <iostream>
+#define nmax 1000005
+using namespace std;
+
+int a[nmax]={},b[nmax]={},c[2*nmax]={};
+
+int main() {
+  int m,n,i,j,k;
+  cin>>m>>n;
+  for(i=1;i<=m;i++){
+      cin>>a[i];
+      }
+      for(i=1;i<=n;i++){
+          cin>>b[i];
+      }
+      i=1;
+      j=1;
+      k=1;
+      if(a[i]<b[j]){
+         c[k]=a[i];
+         i++;
+      }
+      else{
+          c[k]=b[j];
+          j++;
+      }
+      while(i<=m && j<=n){
+          while(i<=m && a[i]==c[k]){
+              i++;
+          }
+          while(j<=n && b[j]==c[k]){
+              j++;
+          }
+          if(i>m || j>n){
+              break;
+          }
+          if(a[i]<b[j]){
+              if(a[i]%2!=c[k]%2){
+              k++;
+              c[k]=a[i];
+              }
+              i++;
+          }
+          else if(a[i]>b[j]){
+              if(b[j]%2!=c[k]%2){
+              k++;
+              c[k]=b[j];
+              }
+              j++;
+          }
+          else if(a[i]==b[j]){
+              if(a[i]%2!=c[k]%2){
+              k++;
+              c[k]=a[i];
+              }
+              i++;
+              j++;
+          }
+      }
+      cout<<a[i]<<endl;
+    if(j>n){// nu mai am elemente in b de prelucrat
+       for(int poz=i;poz<=m;poz++){
+           if(a[poz]!=c[k]){
+               if(a[poz]%2!=c[k]%2){
+               k++;
+               c[k]=a[poz];
+               }
+           }
+       } 
+    } 
+    if(i>m){
+        for(int poz=j;poz<=n;poz++){
+            if(b[poz]!=c[k]){
+                if(b[poz]%2!=c[k]%2){
+                k++;
+                c[k]=b[poz];
+                }
+            }
+        }
+    }
+    for(i=1;i<=k;i++){
+        cout<<c[i]<<" ";
+    }
+}
