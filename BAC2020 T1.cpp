d2c15b8a6d313bf5fc2ddf3581eb08e1b538991c
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-01-06 08:52:08 +0200

    Update BAC2020 T1.cpp

--- BAC2020 T1.cpp
+++ BAC2020 T1.cpp
@@ -19,3 +19,33 @@ int main() {
     }
     return 0;
 }
+
+///SIII ex 3
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+   int a,b,c;
+   ofstream cout("bac.txt");
+   cin>>b>>c;
+   cout<<c<<" "<<b<<" ";
+   if(c==1 && b==1){
+       return 0;
+   }
+   while(b>1){
+       a=3*b-c;
+       c=b;
+       b=a;
+       cout<<a<<" ";
+   }
+   cout<<1;
+    return 0;
+}
+/*
+Algoritmul este eficient dpdv al timplului de executie deoarece are o complexitate O(log3(n)) unde n repr. ultimul termen citit.
+Am rezolvat ecuatia data cu necunoscuta f(n-2) si ne folosim de relatia rezultata pentru a genera 
+termenii anteriori.
+Algoritmul este eficient dpdv al memoriei deoarece am folosit doar 3 variabile intregi simple.
+*/
