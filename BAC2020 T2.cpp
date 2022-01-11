9d1ebe236874089b7bf121620dda5da9007ee352
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-01-11 08:32:41 +0200

    Update BAC2020 T2.cpp

--- BAC2020 T2.cpp
+++ BAC2020 T2.cpp
@@ -16,3 +16,28 @@ int main() {
     fii(n);
     return 0;
 }
+
+///SIII ex 2
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+int main() {
+    char s[101],a[101]={};
+    cin.getline(s,101);
+    char *p;
+    p=strtok(s," ");
+    while(p!=NULL){
+        if(p[0]!='-'){
+            strcat(a,p);
+        }
+        strcat(a," ");
+        p=strtok(NULL," ");
+    }
+   strcpy(s,a);
+   cout<<s;
+    
+   
+    return 0;
+}
