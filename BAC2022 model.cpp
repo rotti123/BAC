629b4a4a4b1fd89643f14bc7392067fad517f9c6
alexiacorb <95295192+alexiacorb@users.noreply.github.com>
2022-01-11 18:30:34 +0200

    Update BAC2022 model.cpp

--- BAC2022 model.cpp
+++ BAC2022 model.cpp
@@ -1,1 +1,29 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i=1,st[10]={},dr[10]={},x,lun,lunmax=0;
+    while(cin>>x){
+        if(st[x]==0){
+            st[x]=i;
+        }
+        dr[x]=i;
+        i++;
+    }
+    for(i=0;i<=8;i=i+2){
+        lun=dr[i]-st[i]+1;
+        if(lun>lunmax){
+            lunmax=lun;
+        }
+    }
+    cout<<lunmax<<endl;
+    for(i=0;i<=8;i=i+2){
+        lun=dr[i]-st[i]+1;
+        if(lun==lunmax){
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
