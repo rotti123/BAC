68baa146739756f7c9a4cbddcd0123725236602a
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-01-17 17:02:48 +0200

    Create BAC2021 sept.cpp

---
+++ BAC2021 sept.cpp
@@ -0,0 +1,17 @@
+
+SIII ex1
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=n;i>=1;i-- ){
+        cout<<i*i*i<<" ";
+    }
+    return 0;
+}
+
