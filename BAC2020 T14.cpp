913ade92a72bd5b8be34740fc007882f2f8343e0
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-01-19 16:27:32 +0200

    Update BAC2020 T14.cpp

--- BAC2020 T14.cpp
+++ BAC2020 T14.cpp
@@ -59,6 +59,33 @@ for (int i = 0; i < 20; i++){
             }
         }
     }
+    Var 2 de la SII ex3.
+	#include <iostream>
+	using namespace std;
+
+	int main() {
+	    int a[5][7],i,j;
+	    for(j=0;j<7;j++){
+		a[0][j]=1;
+	    }
+	    for(i=0;i<5;i++){
+		a[i][6]=1;
+	    }
+
+	    for(i=1;i<5;i++){
+		    for(j=5;j>=0;j--){
+			a[i][j]=(a[i][j+1] + a[i-1][j])%10;
+		    }
+	    }
+
+	    for(i=0;i<5;i++){
+		    for(j=0;j<7;j++){
+			    cout<<a[i][j]<<" ";
+		    }
+		    cout<<endl;
+	    }
+	    return 0;
+	}	    
 	
 Sub III
 
