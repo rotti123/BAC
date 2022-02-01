56afb8a4e6655509111abbf604aa1e39c8d45bde
Ilea-Robert <59930699+Ilea-Robert@users.noreply.github.com>
2022-02-01 18:44:17 +0200

    Update BAC2020 T11.cpp

--- BAC2020 T11.cpp
+++ BAC2020 T11.cpp
@@ -1,3 +1,67 @@
+S1:
+1.d 
+2.d
+3.a
+4.b
+5.c
+
+S2:
+1. 
+
+a)10
+b)1111, 1110, 1100
+c)
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,m=0,c;
+    cin>>n;
+    if(n==0) m=10;
+    else{
+        do{
+            c=n%10;
+            n/=10;
+            if(c>=m){
+                m=c;
+            }
+            else{
+                m=10;
+            }
+        }
+        while(n!=0);
+    }
+    cout<<m;
+    return 0;
+}
+
+d) citeste n (numar natural)
+   m<-0
+   daca n=0 atunci
+	m<-10
+   sfarsit daca
+   altfel
+      cat timp n≠0 executa
+	  c<-n%10
+	  n<-[n/10]
+	  daca c>=m atunci
+		m<-c
+	  sfarsit daca
+	  altfel
+  	      m<-10
+	  sfarsit altfel
+      sfarsit cat timp
+   sfarsit altfel
+   scrie m
+2.
+
+struct sera{
+	int numar;
+    struct{
+		char denumireStiintifica[20],denumirePopulara[20]
+	}specie[100];
+}s;
 
 ///SII ex 3
 #include <iostream>
@@ -57,4 +121,37 @@ int main() ///antetul functiei main
   cin >> x >> y;
   patrate (x, y);
 }
+
+ex 3:
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x,k=1,n;
+    cin>>n;//citim primnul numar
+    while(cin>>x){ //incepem sa citim de la al doilea numar
+        if(n==x) k++;//daca gasim doua numere egale crestem aparitia acestora
+        else{
+            cout<<n<<" "<<k<< " ";//afisam numarul si de cate ori apare
+            k=1;//resetam aparitia pentru a putea compara iar
+        }
+        n=x;//n devine urmatorul numar
+    }
+    cout<<n<<" "<<k;
+    return 0;
+}
+/* Programul este eficient din punct de vedere al timpului de executie 
+   avand o complexitate O(x).
+   Programul este eficient din punct de vedere al memoriei deoarece am
+   utilizat doar 3 variabile simple de tip intreg.
+   Programul executa urmatorii pasi:
+   -se citeste primul numar din sir, dupa care incepem citirea repetata de la
+   al doilea numar.
+   -verifica daca primul numar citit este egal cu al doilea, in caz afirmativ
+   ii crestem aparitia,iar in caz negativ afisam numarul si cifra k , unde k 
+   reprezinta numarul de aparitii al acestuia.
+   -la final schimbam valoarea lui n cu valoarea urmatoare.
+   /*
+
 
