d201ab314fc33ea5f2fc6548937ffc4775da5988
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-02-14 19:07:22 +0200

    Update BAC2021 SS.cpp

--- BAC2021 SS.cpp
+++ BAC2021 SS.cpp
@@ -2,6 +2,26 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+
+int main() {
+  ifstream cin("test.in");
+  int v[11]={},i,x, suma=0;
+  while(cin>>x){
+    if(v[x]==0){
+      suma=suma+10;
+    }
+    v[x]++;
+  }
+  for(i=1;i<=10;i++)
+    suma=suma+i*i*v[i];
+  cout<<suma;
+}
+
+
+///SIII ex 3
+#include <iostream>
+#include <fstream>
+using namespace std;
 int main() {
   ifstream cin("bac.in");
   ofstream cout("bac.out");
