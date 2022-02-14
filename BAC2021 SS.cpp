1d7f173073bbc68fdd015fd63921a28981a1957e
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-02-14 19:43:43 +0200

    Update BAC2021 SS.cpp

--- BAC2021 SS.cpp
+++ BAC2021 SS.cpp
@@ -1,3 +1,36 @@
+///SIII ex 1
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int putere(int n){
+  int i=2,sol,pmin=10000000;
+  while(n!=1)
+  {
+    int p=0;
+    while(n%i==0){
+      n=n/i;
+      p++;
+    }
+    if(p<pmin&&p!=0){
+      pmin=p;
+      sol=i;
+    }
+    i++;
+  }
+  return sol;
+
+}
+int main() {
+  ifstream cin("test.in");
+  int n;
+  cin>>n;
+  cout<<putere(n);
+ 
+}
+
+
+
 ///SIII ex 3
 #include <iostream>
 #include <fstream>
