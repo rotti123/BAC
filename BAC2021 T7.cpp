25584e978851af3e63d14d64aa30f18c4eb66fc4
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-02-16 15:34:20 +0200

    Update BAC2021 T7.cpp

--- BAC2021 T7.cpp
+++ BAC2021 T7.cpp
@@ -28,3 +28,31 @@ int main() {
     
     return 0;
 }
+
+///SIII ex 2
+#include <iostream>
+using namespace std;
+int main() {
+  int x, aux=0, n=0,a[11][11]={};
+  cin>>x;
+  aux=x;
+  while(aux!=0){
+    n++;
+    aux=aux/10;
+  }
+  for(int j=n; j>=1; j--){
+    for(int i=n; i>=1; i--){
+      a[i][j]=x%10;
+    }
+    x=x/10;
+  }
+  for(int i=1; i<=n; i++){
+    for(int j=1;j<=n;j++){
+      cout<<a[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+}
+
+
+///  ¯\_(ツ)_/¯
