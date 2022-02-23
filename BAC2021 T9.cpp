4e2c0faf3057d8a61cf0d0c7351026484e848170
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-02-23 17:33:51 +0200

    Update BAC2021 T9.cpp

--- BAC2021 T9.cpp
+++ BAC2021 T9.cpp
@@ -20,4 +20,33 @@ int main() {
   cout<<nr;
 }
  
+ SIII ex 2
+   #include <fstream>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+  ifstream cin("bac.txt");
+  char s[101], *p, voc[6]="aeiou";
+  int k=0, ok=0;
+  cin.getline(s,100);
+  p=strtok (s," "); //am gasit primul cuvant
+  while (p!=NULL){
+  k=0;
+    for (int i=0; i<=4; i++){
+      if (strchr(p,voc[i])!= NULL){
+        k++;
+      }
+    }
+    if(k==1){
+       cout<<p<<"\n";
+      ok=1;
+      }
+    p=strtok (NULL," "); // preluam urm cuvant
+  }
+  if (ok==0)
+    cout<<"nu exista";
+}
+ 
  
