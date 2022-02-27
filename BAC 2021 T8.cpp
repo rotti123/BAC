c3b54167360c2b9ac595b04e32a19f6e0c3ba46b
mariusel991 <95760306+mariusel991@users.noreply.github.com>
2022-02-27 19:45:01 +0200

    Create BAC 2021 T8.cpp

---
+++ BAC 2021 T8.cpp
@@ -0,0 +1,79 @@
+SI
+
+1 c
+2 c
+3 b
+4 c
+5 d
+
+SII.
+ 
+1. a) 440
+   b) 6,7
+   c)
+  
+  #include <iostream>
+  using namespace std;
+  int main(){
+    int x,n;
+    cin >> n;
+    x=0;
+    for(int i=1;i<=n;i++){
+      if(i%2==0) x=x+i*i;
+      else       x=x+(i+1)*(i+1);
+    }
+    cout<<x;
+     return 0; 
+    }
+
+    d)
+citește n (număr natural)
+x <- 0
+i <- 1      
+┌cat timp i<=n execută
+│ ┌dacă i%2=0 atunci
+│ │ x <- x+i*i
+│ │altfel
+│ │ x <- x+(i+1)*(i+1)
+│ └■
+  i <- i+1
+└■
+scrie x
+
+2)
+  strcut clasa{
+   float sem1,sem2;
+   int elev[40], numar;
+   }p;
+
+3)
+7volt
+  
+ SIII
+ 1)
+  void nrfp(int n, int &m){
+    int d,k=0,e,nr,kmax=-1;
+    m=-1;
+    for(int i=2;i<=n;i++){
+        k=0;
+        d=2;
+        nr=i;
+        while(nr!=1){
+            e=0;
+            while(nr%d==0){
+                nr=nr/d;
+                e++;
+            }
+            if(e>0) k++;
+            d++;
+        }
+        if(k>=kmax){
+            m=i;
+            kmax=k;
+        }
+    }
+}
+
+2)
+  
+  
