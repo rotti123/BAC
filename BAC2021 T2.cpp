29550918997bb1f3c000df92f43be559c112409a
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-03-22 13:01:43 +0200

    Update BAC2021 T2.cpp

--- BAC2021 T2.cpp
+++ BAC2021 T2.cpp
@@ -38,3 +38,50 @@ int main() {
   //cin>>n>>m;
  // cout<<factori(n,m);
 }
+
+///SIII ex 3
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+//ifstream f("bac1.txt");
+//ifstream g("bac2.txt");
+
+int factori(int n,int m){
+  int d=2,k=0,p1,p2;
+  while(n>1 && m>1){
+    p1=0;
+    p2=0;
+    while(n%d==0){
+      n=n/d;
+      p1++;
+    }
+    while(m%d==0){
+      m=m/d;
+      p2++;
+    }
+    if(p1==p2 && p1!=0){
+      k++;
+    }
+     d++; 
+  }
+  return k;
+}
+
+int main() {
+  ifstream cin("bac.txt");
+  int x,p1=-1,p2=-1,k=0;
+  while(cin>>x){
+    k++;
+    if(x>0){
+      if(p1==-1){
+        p1=k;
+      }
+      p2=k;
+    }
+  }
+  if(p2>k-p1+1){
+    cout<<p2;
+  }
+  else cout<<k-p1+1;
+}
