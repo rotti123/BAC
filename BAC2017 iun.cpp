72f26568b3918772f7ed63ec1620e9f04d6799aa
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-04-06 17:57:50 +0300

    Update BAC2017 iun.cpp

--- BAC2017 iun.cpp
+++ BAC2017 iun.cpp
@@ -11,3 +11,21 @@ for(j=1;j<=m;j++){
 
 SIII ex 2
   f(1,1000)=f(5,200)=f(25,40)
+
+SIII ex 4
+void duplicare(int &n){
+    int nr=0, p=1, c;
+    while(n>0){
+      c=n%10;
+      n=n/10;
+      if(c%2==0){
+        nr=nr+p*c/2;
+        p*=10;
+
+      }
+      nr=nr+p*c;
+      p*=10;
+    }
+    n=nr;
+
+  }
