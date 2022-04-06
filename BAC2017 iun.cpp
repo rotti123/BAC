5fcb75d2d842e5e1ee7b431e3b93a67652c7139e
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-04-06 17:49:42 +0300

    Create BAC2017 iun.cpp

---
+++ BAC2017 iun.cpp
@@ -0,0 +1,13 @@
+
+SII ex 5
+  for(int i=1;i<=n;i++){
+	a[i][1]=a[n][m];
+	a[i][m]=a[n][m];
+}
+for(j=1;j<=m;j++){
+	a[1][j]=a[n][m];
+	a[n][j]=a[n][m];
+}
+
+SIII ex 2
+  f(1,1000)=f(5,200)=f(25,40)
