b475c39679750e50b503334294cbb8f366847982
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-04-13 16:09:38 +0300

    Update BAC2017 aug.cpp

--- BAC2017 aug.cpp
+++ BAC2017 aug.cpp
@@ -99,3 +99,62 @@ int main(){
   return 0;
 }
 
+
+SIII ex 4 VAR II cu explicatii
+#include <fstream>
+#include <iostream>
+using namespace std;
+/*
+Caz I prioritate 1
+     x  m1 m2
+     m1 m2
+   m2=m1;
+   m1=x;
+
+Caz II prioritate 2
+    m1  x  m2
+        m2
+    m2=x;
+
+30 80 130 180 230 280 331
+m1 m2
+   a  b
+      a   b
+  */
+
+int main(){
+  ifstream cin("bac.in");
+ int v[1001]={},min1=1001,min2=1001,max=-1,r,i,x,a,b,ok=1;
+  while(cin>>x)
+    {
+      v[x]=1;
+      if(x<=min1)
+      {
+        min2=min1;
+        min1=x;
+      }
+      else if(x<min2)
+      {
+        min2=x;
+      }    
+      if(x>max)
+        max=x;
+    }
+  r=min2-min1;
+  a=min2;
+  for(b=a+1;b<=max;b++)
+    {
+      if(v[b]==1)
+      {
+        if(r!=b-a){
+          ok=0;
+          break;
+          }
+        a=b;
+      }
+    }
+  if(!ok)
+    cout<<"nu";
+  else
+    cout<<r;
+}
