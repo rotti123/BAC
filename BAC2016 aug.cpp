472a98cb7eafda3299b8a51e14bbb7973688a28c
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-04-13 17:32:58 +0300

    Update BAC2016 aug.cpp

--- BAC2016 aug.cpp
+++ BAC2016 aug.cpp
@@ -16,3 +16,49 @@ void duplicare(int &n){
     n=nr;
 
   }
+
+
+SIII ex 4
+    #include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+/** C1)
+   m2  m1  x   (x>=m1)
+       m2  m1
+    m2=m1;
+    m1=x;
+   C2)
+   m2 x m1  (x>m2)
+   m2=x;
+  */
+int main()
+{
+    ifstream cin("bac.txt");
+  unsigned int x, k=0, maxx1=0,maxx2=0,cnt=0;
+  while(cin >> x){
+    if(x%2==1){
+      k++;
+    }
+    if(k==3){
+      if(x%2==0){
+        if(x>=maxx1){
+          maxx2=maxx1;
+          maxx1=x;
+          cnt++;
+        }
+        else if(x>maxx2){
+          cnt++;
+          maxx2=x;
+        }
+      }
+    }
+    if(k>3) break;
+  }
+  if(cnt<2){
+    cout<<"Nu exista";
+  }
+  else{
+    cout<<maxx2<<" "<<maxx1;
+  }
+}
