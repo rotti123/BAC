fa30e7c3c170710fe91d5947f0d3f1492921c61e
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-05-03 12:04:20 +0300

    Update BAC2017 iun.cpp

--- BAC2017 iun.cpp
+++ BAC2017 iun.cpp
@@ -1,4 +1,24 @@
 
+SI ex 2 c)
+citeşte n 
+ (număr natural nenul)
+i<-1
+┌cat timp i<=n execută
+│┌pentru j1,n execută
+││┌dacă i=j sau i+j=n+1 atunci 
+│││ scrie ’#’ 
+│││altfel 
+│││ scrie j 
+││└■
+│└
+|i<-i+1
+└■
+
+SII ex 3
+cout<<x.nume[0]<<endl<<(x.media1+x.media2)/2;
+
+
+
 SII ex 5
   for(int i=1;i<=n;i++){
 	a[i][1]=a[n][m];
@@ -12,7 +32,7 @@ for(j=1;j<=m;j++){
 SIII ex 2
   f(1,1000)=f(5,200)=f(25,40)
 
-SIII ex 4
+SIII ex 3
 void duplicare(int &n){
     int nr=0, p=1, c;
     while(n>0){
@@ -29,3 +49,36 @@ void duplicare(int &n){
     n=nr;
 
   }
+
+SIII ex 4
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  int x,lungMax=0,lung=0,cnt=0;
+  ifstream cin("bac.txt");
+  while(cin>>x){
+    if(x%2==0){
+      lung++;
+    }else{
+      if(lung>lungMax){
+        lungMax=lung;
+        cnt=1;
+      }else if(lung == lungMax){
+        cnt++;
+      }
+      lung=0;
+    }
+  }
+  ///tratez ultima secv
+  if(lung>lungMax){
+        lungMax=lung;
+        cnt=1;
+      }else if(lung == lungMax){
+        cnt++;
+      }
+  cout<<cnt;
+}
