41d2cc93ae44e09fc63b2cf8e7086693da1fb67e
cosmindemian <75207153+cosmindemian@users.noreply.github.com>
2022-06-09 12:07:25 +0300

    Update BAC2014 SS.cpp

--- BAC2014 SS.cpp
+++ BAC2014 SS.cpp
@@ -5,28 +5,28 @@ using namespace std;
 
 int main()
 {
-    int a,b,k=1,lmax=0,val;
-      cin>>a;
-      while(cin>>b){
-        if(a==b){
-          k++;
-          if(k>=lmax){
-            lmax=k;
-            val=b;
-          }
-        }
-        else{
-          k=1;
-          if(k>=lmax){
-            lmax=k;
-            val=b;
-          }
-        }
-        a=b;
+  int a,b,k=1,lmax=0,val;
+  cin>>a;
+  while(cin>>b){
+    if(a==b){
+      k++;
+    }
+    else{
+      k=1;
+      if(k>=lmax){
+        lmax=k;
+        val=b;
       }
-      cout<<lmax<<'\n';
-      for(int i=1;i<=lmax;i++){
-        cout<<val<<" ";
-      }
+    }
+    a=b;
+  }
+  if(k>=lmax){
+    lmax=k;
+    val=b;
+  }
+  cout<<lmax<<'\n';
+  for(int i=1;i<=lmax;i++){
+    cout<<val<<" ";
+  }
     return 0;
 }
