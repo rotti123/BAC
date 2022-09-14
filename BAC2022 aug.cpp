9bcc6596e16a1bfdd790d6312841e71e95476a73
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-09-14 17:39:06 +0300

    Update BAC2022 aug.cpp

--- BAC2022 aug.cpp
+++ BAC2022 aug.cpp
@@ -19,3 +19,14 @@ else cout << "NORMAL";
  
   return 0;
 }
+
+III.1
+   void schimb_cata(int &n, int x, int p){
+  int e=1,st,dr;
+  for(int i=1; i<=p; i++){
+    e=e*10;
+  }
+  dr=n%e;
+  st=n/e;
+  n=(st/10*10+x)*e+dr;
+}
