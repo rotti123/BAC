3cdb59d341f64f4fdc158d6266c98a16d40a2448
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-10-12 17:43:02 +0300

    Update BAC2020 T5.cpp

--- BAC2020 T5.cpp
+++ BAC2020 T5.cpp
@@ -2,20 +2,33 @@
 ///SIII ex 3
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
-int main() {
-    ifstream cin("bac.txt");
-    int x,s=0,smax=-10000000;
-   while(cin>>x){
-        if(s<0){
-                s=x;
-        }
-        else s=s+x;
-        if(s>smax){
-                smax=s;
-        }
-   }
-   cout<<smax;
-    return 0;
+int main ()
+{
+  ifstream cin("bac.txt");
+  ofstream cout("bac.out");
+  int x,s=0,Smax=-100000000;
+  while(cin>>x){
+    if(s>=0){
+     s=s+x;
+    }
+    else{
+      s=x;
+    }
+    if(s>Smax){
+      Smax=s;
+    }
+     
+  }
+  cout<<Smax;
 }
+
+/* Programul este eficient dpdv al timpului de exec
+deoarece are o complexitate O(n), unde n repr nr de elemente ale sirului  
+Programul este eficient dpdv al mem deoarece am folosit doar trei var intregi simple.
+
+Descriere algoritm
+Suma curenta se actualizeaza astfel daca suma anterioara este negativa, suma va deveni ultimul element citit, altfel se adauga elementul curent la suma. 
+*/
