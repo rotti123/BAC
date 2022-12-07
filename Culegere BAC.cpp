60eb87b15c828522310c1353915797f4a7a3563f
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-12-07 08:56:00 +0200

    Update Culegere BAC.cpp

--- Culegere BAC.cpp
+++ Culegere BAC.cpp
@@ -61,3 +61,125 @@ int main(){
   cout<<sol;
 }
 
+Pr.5 /107
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+  char s[81];
+  int i=0;
+  cin.getline(s,81);
+  char *p=strtok(s," ");
+  while (p!=NULL) {
+    cout << p << " ";
+    p=strtok(NULL," ");
+  }
+  return 0;
+}
+
+Pr. 6/107
+
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+  char a[251],b[251],c[505]={};
+  cin >> a >> b;
+  int i=0,j=0,k=0;
+  while(i<strlen(a)&&j<strlen(b)){
+    if(a[i]<b[j]){
+        c[k]=a[i];
+        k++;
+        i++;
+    }
+    else if(b[j]<a[i]){
+        c[k]=b[j];
+        k++;
+        j++;
+    }
+    else{
+        c[k]=a[i];
+        k++;
+        i++;
+        c[k]=b[j];
+        k++;
+        j++;
+    }
+  }
+
+  if(i<strlen(a)){
+    for(int x=i;x<strlen(a);x++){
+        c[k]=a[x];
+        k++;
+    }
+  }
+
+  if(j<strlen(b)){
+    for(int x=j;x<strlen(b);x++){
+        c[k]=b[x];
+        k++;
+    }
+  }
+
+  cout << c;
+  return 0;
+}
+
+/*
+ampstz
+bfgostx
+*/
+
+Pr. 9/108
+#include <iostream>
+#include <cstring>
+using namespace std;
+int vocale(char s[]){
+int k=0;
+for(int i=0 ; i<strlen(s); i++){
+    if(strchr("aeiou",s[i])){
+        k++;
+    }
+}
+return k;
+}
+int main(){
+  char s[200];
+  cin.getline(s,200);
+  char *p=strtok(s," ");
+  while(p!=NULL){
+    if(vocale(p)<=3){
+        cout<<p<<endl;
+    }
+    p=strtok(NULL," ");
+  }
+  return 0;
+}
+
+
+
+Pr. 10/108 (de completat)..
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+  char s[200],t[50];
+  cin.getline(s,200);
+  for(int i=0;i<strlen(s);i++){
+    if(strchr("aeiou",s[i])!=NULL){
+        strcpy(t,s+i+1);
+        strcpy(s+i,t);i--;
+    }
+    ....
+  }
+  return 0;
+}
+
+
+
