77e5d98d78894509a2e5d09396389396c041b6fc
rotti123 <92742818+rotti123@users.noreply.github.com>
2023-01-18 17:05:43 +0200

    Update BAC2021 T6.cpp

--- BAC2021 T6.cpp
+++ BAC2021 T6.cpp
@@ -27,3 +27,64 @@ int main() {
     cout<<m;
     return 0;
 }
+
+
+SIII ex 3
+    
+    #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  ifstream cin("bac.txt");
+  int x, a, b, c, k=0, kn=0;
+  /// k= contorul pentru elem curent din sir
+  /// kn= contor numere negative
+  //a= poz primului element negativ din sir
+  //b= poz II   -||-
+  //c= poz III   -||-
+  while(cin>>x){
+    k++;
+    if(x<0){
+      kn++;
+      if(kn==1){
+        a=k;
+      }
+      if(kn==2){
+        b=k;
+      }
+      if(kn==3){
+        c=k;
+      }
+    }
+  }
+  int l1, l2, l3;
+  l1=b-1;
+  l2=(c-1)-(a+1)+1;
+  l3=k-(b+1)+1;
+  cout<<max(l1,max(l2,l3));
+  return 0;
+}
+
+/*
+Algoritmul citeste din fisier sirul dat, verificand daca  numarul citit este negativ. In acest caz retinem pozitia acestuia. Presupunem ca avem numerele negative pe poz a, b si c, si lungimea sirului este k. Atunci vom alege lungimea maxima dintre urmatoarele secvente: 
+L1=b-1
+L2=(c-1) -(a+1)+1
+L3=k-(b+1)+1
+
+
+
+15 21 -61 9 870 -23 11 5 8 -81  5 14
+
+1  2   3  4  5   6  7  8 9  10 11 12
+       a         b          c
+
+S1  1..5  L1=b-1
+S2  4..9  L2=(c-1) -(a+1)+1
+S3  7..12 L3=k-(b+1)+1
+
+cout<<max(l1,max(l2,l3))
+
+*/
