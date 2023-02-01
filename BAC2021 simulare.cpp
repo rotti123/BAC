3b4a49ecc334e06dbcd91508da90e8c75550e554
rotti123 <92742818+rotti123@users.noreply.github.com>
2023-02-01 17:54:03 +0200

    Update BAC2021 simulare.cpp

--- BAC2021 simulare.cpp
+++ BAC2021 simulare.cpp
@@ -1,3 +1,66 @@
+///SIII ex 1
+
+#include <iostream>
+using namespace std;
+int putere(int n)
+{
+  int d=2,p, pmin=1000000, dmin;
+  while(n>1)
+    {
+      p=0;
+      while(n%d==0)
+        {
+          p++;
+          n=n/d;
+        }
+      if(pmin>=p && p!=0)
+      {
+        pmin=p;
+        dmin=d;
+      }
+      d++;
+    }
+  return dmin;
+}
+int main() {
+
+int n;
+cin>>n;
+cout<<putere(n);
+}
+
+
+///SIII ex 2
+#include <cmath>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main() {
+//  ifstream cin("puteri6.in");
+//  ofstream cout("puteri6.out");
+  int n,a[21][21],k,aux;
+  cin>>n;
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+      cin>>a[i][j];
+    }
+  }
+  cin>>k;
+  aux = a[k][k-1];
+  for(int j = k - 1; j > 1; j --){ ///permutare circulara spre dreapta
+      a[k][j] = a[k][j - 1];
+  }
+  a[k][1] = aux;
+  for(int i = 1; i <= n; i ++){
+    for(int j = 1; j <= n; j ++){
+      cout << a[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+
 ///SIII ex 3
 #include <iostream>
 #include <fstream>
