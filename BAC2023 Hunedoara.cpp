e06fa8dd115b0ba64a89165795f15abf69e8defe
rotti123 <92742818+rotti123@users.noreply.github.com>
2023-03-22 19:20:38 +0200

    Update BAC2023 Hunedoara.cpp

--- BAC2023 Hunedoara.cpp
+++ BAC2023 Hunedoara.cpp
@@ -35,3 +35,43 @@ int main()
 }
 
 SIII.2
+  #include <iostream>
+#include <cstring>
+using namespace std;
+
+
+int main()
+{
+    int  lung=0, lmin=1000, st=-1, dr=-1,cod;
+    char s[101];
+    cin.getline(s,101);
+
+  for(int i=0; i<=strlen(s)-5; i++)
+    for(int j=i+4; j<strlen(s); j++)
+      {
+        int v[10]={},ok=1; ///vect de aparitii
+        for(int k=i; k<=j; k++)
+          if(strchr("13579", s[k])){
+            cod=s[k]-'0';
+            v[cod]=1;
+          }
+        for(int poz=1; poz<=9; poz+=2)
+          {
+            if(v[poz]==0)
+              ok=0;
+          }
+        lung=j-i+1;
+        if(lung<=lmin && ok)
+        {
+          lmin=lung;
+          st=i;
+          dr=j;
+        }
+        
+      }
+  if(st!=-1)
+    for( int i=st; i<=dr; i++)
+      cout<<s[i];
+  else cout<<"nu";
+}
+
