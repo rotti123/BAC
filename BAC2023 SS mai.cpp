b1b734179d22d0f24000fe1185e47acf21ec8ee2
rotti123 <92742818+rotti123@users.noreply.github.com>
2023-06-07 17:31:03 +0300

    Update BAC2023 SS mai.cpp

--- BAC2023 SS mai.cpp
+++ BAC2023 SS mai.cpp
@@ -18,3 +18,45 @@ int main() {
     }
     return 0;
 }
+SIII.1
+  #include <iostream>
+#include <string.h>
+using namespace std;
+
+void Putere(int n, int &x, int &p){
+  for(int i=2; i<=n; i++){
+    int a=n;
+    p=0;
+    while(a%i==0){
+      a=a/i;
+      p++;
+    }
+    if(a==1){
+      x=i;
+      break;
+    }
+  }
+}
+
+
+///var Andrei (－_－) zzZ
+void putere(int n, int &x, int &p){
+    int val;
+    for(x=2; x<=1000; x++){
+        val=1;
+        for(p=2; p<=1000; p++){
+            val=x*val; ///val=x^p
+            if(val==n) return;
+            else if(val>n) break;
+        }
+    }
+}
+
+int main() {
+    int n, x, p;
+  n=216;
+      Putere(n,x,p);
+  cout<<x<<" "<<p;
+    return 0;
+}
+
