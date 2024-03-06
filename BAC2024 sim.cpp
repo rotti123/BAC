e292ec62edee947a8c6fc7ea0bf8bb6323a5a81e
rotti123 <92742818+rotti123@users.noreply.github.com>
2024-03-06 17:57:57 +0200

    Update BAC2024 sim.cpp

--- BAC2024 sim.cpp
+++ BAC2024 sim.cpp
@@ -33,3 +33,49 @@ int n;
   cout<<Impare(n);
 
 }
+
+SIII ex 2
+  #include <iostream>
+#include <cstring>
+using namespace std;
+
+
+int main()
+{
+ int n,lung,ok=1;
+  char s[101],cuv[100][11],*p, sol[101]={};
+cin>>n;
+cin.ignore();///ignor Enter dupa n
+  for(int i=0;i<n;i++){
+    cin>>cuv[i];
+  }
+  cin.ignore(); ///ignor Enter dupa ultimul cuvant
+  cin.getline(s,100);
+  p=strtok(s," ");
+  while(p!=NULL){
+    lung=strlen(p);
+    if(p[0]=='*'){ ///caut cuvant pentru masca *****
+      ok=0; //pp ca nu am gasit cuvantul de lungime lung
+      for(int i=0;i<n;i++){
+        if(strlen(cuv[i])==lung){
+          strcat(sol,cuv[i]);
+          strcat(sol," ");
+          ok=1;
+          break;
+        }
+      }
+      if(ok==0){
+        cout<<"imposibil";
+        return 0;
+      }
+    }
+    else{
+      strcat(sol,p);
+      strcat(sol," ");
+    }
+    p=strtok(NULL," ");
+  }
+  cout<<sol;
+  return 0;
+}
+
