7a0edb0ff90601cfe417a054223f64189b29e366
rotti123 <92742818+rotti123@users.noreply.github.com>
2024-03-25 15:43:17 +0200

    Update BAC2015 sim.cpp

--- BAC2015 sim.cpp
+++ BAC2015 sim.cpp
@@ -39,3 +39,86 @@ while(n!=0){
   }
     
 }
+
+III.4
+
+  BAC 2015 sim
+VAR I - vector de aparitii
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using  namespace std;
+ifstream f("bac.in");
+int main()
+{
+  int ap[10001]={},n,x,y,z,ok=0;
+  f>>n;
+  for(int i=1;i<=n;i++)
+  {
+      f>>z;
+      ap[z]++;
+  }
+  f>>x>>y;
+  for(int i=y;i>=x;i--)
+  {
+      if(ap[i]!=0)
+      {
+          ok=1;
+          cout<<i;
+          break;
+      }
+  }
+  if(ok==0) cout<<"nu exista";
+    return 0;
+}
+
+VAR II - cautare binara
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using  namespace std;
+ifstream f("bac.txt");
+int cb (int a[],int st ,int dr ,int x,int y)
+{
+    int poz=0;
+    while(st<=dr)
+    {
+        int mij=(st+dr)/2;
+        if(x<=a[mij] && a[mij]<= y)
+        {
+            poz=mij;
+            dr=mij-1;
+        }
+        if(a[mij]>y)
+        {
+            st=mij+1;
+        }
+        if(a[mij]<x)
+        {
+            dr=mij-1;
+        }
+
+    }
+    return poz;
+}
+int main()
+{
+  int a[10001]={},n,x,y,z,ok=0;
+  f>>n;
+  for(int i=1;i<=n;i++)
+  {
+      f>>a[i];
+  }
+  f>>x>>y;
+  ok=cb(a,1,n,x,y);
+  if(ok==0) cout<<"nu exista";
+  else
+  {
+      cout<<a[ok]<<" ";
+  }
+    return 0;
+}
+
+
