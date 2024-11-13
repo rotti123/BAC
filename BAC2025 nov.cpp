1aef8e9d9f8e5ddab64d32265bc736b8824ee9f4
rotti123 <92742818+rotti123@users.noreply.github.com>
2024-11-13 17:01:48 +0200

    Update BAC2025 nov.cpp

--- BAC2025 nov.cpp
+++ BAC2025 nov.cpp
@@ -1,4 +1,30 @@
+
+
 SIII
+
+SIII.2
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, a[21][21],k;
+    cin>>k>>n;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            a[i][j]=k*i+i-j;
+           cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+ return 0;
+}
+
+    
 #include <iostream>
 
 using namespace std;
