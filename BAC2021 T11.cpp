4d4e1cddbeb162a6a78bb4a54cefe25296400105
rotti123 <92742818+rotti123@users.noreply.github.com>
2024-11-25 16:58:24 +0200

    Update BAC2021 T11.cpp

--- BAC2021 T11.cpp
+++ BAC2021 T11.cpp
@@ -2,32 +2,37 @@
 
 /// SIII ex1 
 #include <iostream>
- 
+#include <fstream>
 using namespace std;
- 
-void imog(int x, int y , int &rez){
-    int p=1,m=0,n=0,c;
-    while(x!=0){
-        c=x%10;///
-        x/=10;
-        if(c%2==1) m=m*10+c;/// construiesc numarul fara cifrele pare inversat
-    }
-    p=1;
-    while(y!=0){
-        c=y%10;
-        y/=10;
-        if(c%2==1){
-            n=n+c*p;
-            p*=10;
+
+void imog(int x, int y,int &rez)
+{
+    int nrx=0,nry=0;
+    int p=1;
+    while(x)
+    {
+        if(x%2==1)
+        {
+            nrx=nrx+x%10*p;
+            p=p*10;
         }
+        x=x/10;
+
     }
-    if(m==n && m!=0) rez=1;
-    else             rez=0;
+    while(y){
+        if(y%2==1){
+            nry=nry*10+y%10;
+        }
+        y/=10;
+    }
+    if(nrx==nry && nrx!=0) rez=1;
+    else rez=0;
 }
- 
-int main() {
+
+int main()
+{
     int x,y,rez;
-    cin >> x>>y;
+    cin>>x>>y;
     imog(x,y,rez);
     cout<<rez;
     return 0;
