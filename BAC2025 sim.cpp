7f144f9dc18214a9ac32e77450030757ba3f60b3
rotti123 <92742818+rotti123@users.noreply.github.com>
2025-11-06 17:25:52 +0200

    Update BAC2025 sim.cpp

--- BAC2025 sim.cpp
+++ BAC2025 sim.cpp
@@ -1,3 +1,39 @@
+SIII.2
+  #include <iostream>
+#include <cstring>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    char s[101][21];
+    unsigned n,nrv=0,nrvc,ok=0;
+    cin>>n;
+    cin.ignore();
+    for( int i=0;i<n;i++)
+        cin>>s[i];
+    for(int i=0;i<strlen(s[n-1]);i++)
+        if(strchr("aeiou",s[n-1][i]))
+            nrv++;
+    for(int i=0;i<n-1;i++)
+    {
+        nrvc=0;
+        for(int j=0;j<strlen(s[i]);j++)
+        if(strchr("aeiou",s[i][j]))
+            nrvc++;
+        if(strcmp(s[i],s[n-1])!=0 && nrv==nrvc)
+        {
+            ok=1;
+            cout<<s[i]<<" ";
+        }
+
+    }
+    if(ok==0)
+        cout<<"nu exista";
+    return 0;
+
+}
+
 SIII.3
   #include <bits/stdc++.h>
 #include<cstring>
