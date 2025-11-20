2453d83e0204d66afd00d204505dbb9d5d4f27cb
rotti123 <92742818+rotti123@users.noreply.github.com>
2025-11-20 17:29:27 +0200

    Update BAC2026 model.cpp

--- BAC2026 model.cpp
+++ BAC2026 model.cpp
@@ -26,3 +26,39 @@ int main() {
      cout<<tL;
     return 0;
 }
+
+
+SIII. 3
+  #include <iostream>
+
+using namespace std;
+int main()
+{
+  unsigned int n,p=0;
+  cin>>n;
+  for(int i=13;i<=n;i+=13)
+  {
+    int x=i;
+    while(x%13==0)
+    {
+        p++;
+        x/=13;
+    }
+  }
+  cout<<p;
+
+    return 0;
+}
+
+/* Algoritmul este eficient dpdv al memoriei,
+ deoarece am folosit 4 variabile simple.
+Algoritmul este eficient dpdv al memoriei,deoarece
+are o complexitate O(n), unde n este dat in enunt.
+Algoritmul parcurge acei termeni din produsul factorialului
+care sunt multipli de 13. Pentru un astfel de multiplu 
+se calculeaza de cate ori se imparte la 13 si se aduna la
+solutia finala p.
+*/
+
+
+
