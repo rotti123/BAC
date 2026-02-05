2d5a9f069dd3da23ccbb8a28ea41d7b2fce8d085
rotti123 <92742818+rotti123@users.noreply.github.com>
2026-02-05 17:25:47 +0200

    Update BAC2024 HD.cpp

--- BAC2024 HD.cpp
+++ BAC2024 HD.cpp
@@ -23,3 +23,46 @@ int main()
     }
     return 0;
 }
+
+III.2
+    #include <iostream>
+#include <cstring>
+using namespace std;
+int main() {
+    int n,x,y,sol,op=0;
+    char s[21];
+    cin>>n;
+    for(int poz=1;poz<=n;poz++)
+    {
+        x=0; y=0; sol=0;
+        cin>>s;
+        cout<<s;
+        int i=0;
+        while(strchr("+-*",s[i])==NULL)
+        {
+            x=x*10+s[i]-'0';
+            i++;
+        }
+        if(s[i]=='+') op=1;
+        if(s[i]=='-') op=2;
+        if(s[i]=='*') op=3;
+        i++;
+        while(s[i]!='=')
+        {
+            y=y*10+s[i]-'0';
+            i++;
+        }
+        if(op==1) sol=x+y;
+        if(op==2) sol=x-y;
+        if(op==3) sol=x*y;
+        cout<<sol<<endl;
+    }
+    return 0;
+}
+/*
+12*40=
+1228+29=
+1300-1=
+7*11=
+*/
+
