64edbbe6ac9940841feb66dc34ca9ef2e1d75f3d
rotti123 <92742818+rotti123@users.noreply.github.com>
2026-02-26 18:18:18 +0200

    Update BAC2023 Hunedoara.cpp

--- BAC2023 Hunedoara.cpp
+++ BAC2023 Hunedoara.cpp
@@ -34,6 +34,65 @@ int main()
 
 }
 
+SIII 2. 26.02.2026
+  #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+    char s[105];
+    int ap[10]={}, lung, lmin=105, ok, st=105, dr=-1, x=0, y=0;
+void afis(){
+    for(int i=1;i<=9;i+=2){
+        cout<<i<<":"
+        <<ap[i]<<" ";
+    }
+    cout<<'\n';
+    getchar();
+}
+
+int main()
+{
+    for(int i=1;i<=9;i+=2)
+        ap[i]=-1;
+    cin.getline(s,100);
+    for(int i=0;i<strlen(s);i++)
+    {
+        if(strchr("13579",s[i]))
+        {
+            int cod=s[i]-'0';
+            ap[cod]=i;
+            ok=1;
+            st=105, dr=-1;
+            for(int k=1;k<=9;k+=2)
+            {
+                if(ap[k]==-1)
+                    ok=0;
+                if(st>ap[k] && ap[k]!=-1)
+                    st=ap[k];
+                if(dr<ap[k] && ap[k]!=-1)
+                    dr=ap[k];
+
+            }
+            if(ok==1)
+            {
+                if(lmin>=dr-st+1)
+                {
+                    lmin=dr-st+1;
+                    x=st;
+                    y=dr;
+                }
+            }
+            cout<<s[i]<<'\n';
+            afis();
+        }
+    }
+    if(x==0)
+        cout<<"NU EXISTA";
+    else
+        for(int j=x;j<=y;j++)
+          cout<<s[j];
+}
+
+
 SIII.2
   #include <iostream>
 #include <cstring>
